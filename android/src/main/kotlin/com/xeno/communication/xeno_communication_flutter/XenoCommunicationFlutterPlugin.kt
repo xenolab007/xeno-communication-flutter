cd175fc111918f7efbc5792936506666e9432cdc
kshivam1177 <shivam.kumar@xeno.in>
2024-11-22 14:16:40 +0530

    changes

--- android/src/main/kotlin/com/xeno/communication/xeno_communication_flutter/XenoCommunicationFlutterPlugin.kt
+++ android/src/main/kotlin/com/xeno/communication/xeno_communication_flutter/XenoCommunicationFlutterPlugin.kt
@@ -22,12 +22,29 @@ class XenoCommunicationFlutterPlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         if (call.method == "sdk-initialize") {
+            val apiKey = call.argument<String?>("apiKey") as String?
+            print("API Key: $apiKey")
+
             result.success(true)
         } else if (call.method == "user-authenticate") {
+            val phone = call.argument<String?>("phone") as String?
+            val countryCode = call.argument<String?>("countryCode") as String?
+            val email = call.argument<String?>("email") as String?
+            val name = call.argument<String?>("name") as String?
+
+            print("Phone: $phone")
+            print("Country code: $countryCode")
+            print("Email: $email")
+            print("Name: $name")
+
             result.success(true)
         } else if (call.method == "update-device-token") {
+            val token = call.argument<String>("deviceToken") as String
+            print("Device token: $token")
             result.success(true)
         } else if (call.method == "on-message-received") {
+            val msg = call.argument<HashMap<String, Any>>("message") as HashMap<String, Any>
+            print("Message received: $msg")
             result.success(true)
         } else if (call.method == "unset-user") {
             result.success(true)
